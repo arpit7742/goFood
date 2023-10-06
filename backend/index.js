80c7a187ac4ea8259ac5f17f25697b4b67029e8e
ARPIT <agrawalarpit414@gmail.com>
2023-10-07 02:17:52 +0530

    built login and signup page

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,21 @@ const app = express();
 const port = 5000;
 const mongoDB = require('./db'); // Import the mongoDB function
 
-mongoDB(); // Call the mongoDB function to establish the MongoDB connection
+mongoDB() // Call the mongoDB function to establish the MongoDB connection
 
+app.use((req,res, next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+})
+app.use(express.json())
+app.use('/api',require("./Routes/Creatuser")); 
 app.get('/', (req, res) => {
   res.send('Hello World!----');
 });
-app.use(express.json())
-app.use('/api',require("./Routes/Creatuser")); 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,81 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link,useNavigate } from 'react-router-dom';
 export default function Login() {
+  const [credentials, setcredentials] = useState({ email: "", password: "" });
+  let navigate  = useNavigate()
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    console.log(
+      JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      })
+    );
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (!json.success) {
+      alert("Enter valid credinatals");
+    }
+    if (json.success) {
+      navigate("/"); 
+    }
+  };
+  const onChange = (event) => {
+    setcredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
   return (
     <div>
-      Login
+      <div className="conatiner">
+        <form onSubmit={handlesubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={credentials.email}
+              onChange={onChange}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+              id="exampleInputPassword1"
+            />
+          </div>
+
+          <button type="submit" className="m-3 btn btn-success">
+            Submit
+          </button>
+          <Link to="/creatuser" className="m-3 btn btn-danger">
+            i m  a new user
+          </Link>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
